04d9e1737153194d287d357a768567b3a8859b97
danimohamed <mohameddani993@gmail.com>
2023-09-06 02:09:31 +0100

    last_update

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 # Author - Tolulope Fakunle
 
-def print_comb2(n):
-    for i in range(10):
-        for j in range(i + 1, 10):
-            print(f"{i}{j}, ", end="")
-    print()
-
-
-print_comb2(10)
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print("{}{}".format(digit1, digit2))
+        else:
+            print("{}{}".format(digit1, digit2), end=", ")
