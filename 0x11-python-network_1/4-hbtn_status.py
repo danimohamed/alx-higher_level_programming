9b2db441b8a9772c7f53c9e13cbcb6c7bafa9762
mohamed dani <112667449+danimohamed@users.noreply.github.com>
2024-01-06 10:12:03 +0100

    What's my status?

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status."""
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import requests
 
-if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an HTTPError for bad responses
-        print("Body response:")
-        print("\t- type: {}".format(type(response.text)))
-        print("\t- content: {}".format(response.text))
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching {url}: {e}")
 
+if __name__ == "__main__":
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
